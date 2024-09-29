86e761dc57b73aeae52449f1c3f902d87535c282
PedroAS7 <pedrocas-pt@hotmail.com>
2024-09-29 18:11:08 +0200

    Adding message manipulator class

---
+++ src/main/java/ch/heigvd/dai/exceptions/MessageManipulationException.java
@@ -0,0 +1,17 @@
+/*
+ * @brief   [file description]
+ * @class   DAI
+ * @pw      1
+ * @authors Pedro Alves da Silva, Gonçalo Heleno Carvalheiro
+ * Copyright (c) 2024
+ */
+
+package ch.heigvd.dai.exceptions;
+
+public class MessageManipulationException extends IllegalArgumentException
+{
+	public MessageManipulationException(String s)
+	{
+		super(String.format("Exception thrown while manipulating message: %s", s));
+	}
+}
