1775c69d4a250b4fbe88fb15e8aff6a56a10f6f0
Gonçalo Heleno <goncalocheleno@atomp.eu>
2024-10-11 11:36:35 +0200

    chore: run spotless:apply

--- src/main/java/ch/heigvd/dai/exceptions/BmpFileException.java
+++ src/main/java/ch/heigvd/dai/exceptions/BmpFileException.java
@@ -1,17 +1,13 @@
 /**
- * @brief   Exception raised when the BMP file's sanity checks fail
- * @class   DAI
- * @pw      1
- * @authors Pedro Alves da Silva, Gonçalo Heleno Carvalheiro
- * Copyright (c) 2024
+ * @brief Exception raised when the BMP file's sanity checks fail
+ * @class DAI
+ * @pw 1
+ * @authors Pedro Alves da Silva, Gonçalo Heleno Carvalheiro Copyright (c) 2024
  */
-
 package ch.heigvd.dai.exceptions;
 
-public class BmpFileException extends IllegalArgumentException
-{
-	public BmpFileException(String s)
-	{
-		super(String.format("BMP file parsing error: %s", s));
-	}
+public class BmpFileException extends IllegalArgumentException {
+  public BmpFileException(String s) {
+    super(String.format("BMP file parsing error: %s", s));
+  }
 }
