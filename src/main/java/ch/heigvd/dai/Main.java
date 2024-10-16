1a94831ffec7ab291d19c3ff8a704a2ad61e9a83
Gonçalo Heleno <goncalocheleno@atomp.eu>
2024-10-16 18:41:01 +0200

    feat: add output to show execution time

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -11,12 +11,21 @@ public class Main {
         new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
             .getName();
 
-    // Create root command and get exit code
+    // Create root command
+    Root root = new Root();
+
+    // Calculate execution time for root command and its subcommands
+    Long start = System.nanoTime();
     int exitCode =
-        new CommandLine(new Root())
+        new CommandLine(root)
             .setCommandName(jarFilename)
             .setCaseInsensitiveEnumValuesAllowed(true)
             .execute(args);
+    Long end = System.nanoTime();
+
+    if (exitCode == 0) {
+      System.out.println("Execution time: " + (end - start) / (1000 * 1000) + " ms");
+    }
 
     System.exit(exitCode);
   }
