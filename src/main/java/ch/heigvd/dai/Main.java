cf1335f3835873731791d41ada6e8afbf169e8f5
Gonçalo Heleno <goncalocheleno@atomp.eu>
2024-10-16 21:57:33 +0200

    fix: include only the execute function when measuring execution time

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -6,21 +6,20 @@ import picocli.CommandLine;
 
 public class Main {
   public static void main(String[] args) {
+    // Example from class -
+    // https://github.com/heig-vd-dai-course/heig-vd-dai-course-java-ios-practical-content-template/blob/778e1934a64f338e93613afbb31dd9e92356d7c4/src/main/java/ch/heigvd/dai/Main.java#L10
     // Define command name - source: https://stackoverflow.com/a/11159435
     String jarFilename =
         new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
             .getName();
 
-    // Create root command
-    Root root = new Root();
+    // Create root command and CommandLine
+    CommandLine command = new CommandLine(new Root());
+    command.setCommandName(jarFilename).setCaseInsensitiveEnumValuesAllowed(true);
 
     // Calculate execution time for root command and its subcommands
     Long start = System.nanoTime();
-    int exitCode =
-        new CommandLine(root)
-            .setCommandName(jarFilename)
-            .setCaseInsensitiveEnumValuesAllowed(true)
-            .execute(args);
+    int exitCode = command.execute(args);
     Long end = System.nanoTime();
 
     if (exitCode == 0) {
